fba607aa53a501a2b72d04d4656f57f98498f8b8
Charles Broskoski <broskoski@gmail.com>
2017-07-19 19:12:14 -0400

    Fix jest setup and add ChannelName test

---
+++ components/__tests__/ChannelName.js
@@ -0,0 +1,19 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import ChannelNameText from '../ChannelNameText'
+import colors from '../../constants/Colors'
+
+const channel = {
+  title: 'Arena',
+  visibility: 'private',
+}
+
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <ChannelNameText channel={channel} />,
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+  expect(tree.children[0].props.style[1].color).toBe(colors.private)
+})
