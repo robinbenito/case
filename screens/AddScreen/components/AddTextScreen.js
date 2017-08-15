573c1d77d05dbd66c516caa3f216760e957ea7d2
Charles Broskoski <broskoski@gmail.com>
2017-08-15 16:35:47 -0400

    Add KeyboardAvoidingView

--- screens/AddScreen/components/AddTextScreen.js
+++ screens/AddScreen/components/AddTextScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
+  KeyboardAvoidingView,
   StyleSheet,
   View,
 } from 'react-native'
@@ -66,37 +67,39 @@ export default class AddTextScreen extends React.Component {
     ) : null
 
     return (
-      <View style={styles.container}>
-        <FieldSet
-          isFirst
-          style={styles.fieldset}
-          label="Text"
-          onChange={this.onFieldChange}
-          fields={[
-            {
-              key: 'content',
-              placeholder: 'Text',
-              type: 'textarea',
-            },
-          ]}
-        />
-        <FieldSet
-          label="Title / Description"
-          onChange={this.onFieldChange}
-          style={styles.fieldset}
-          fields={[
-            {
-              key: 'title',
-              placeholder: 'Title',
-            },
-            {
-              key: 'description',
-              placeholder: 'Description',
-            },
-          ]}
-        />
-        {bottomButton}
-      </View>
+      <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.container}>
+          <FieldSet
+            isFirst
+            style={styles.fieldset}
+            label="Text"
+            onChange={this.onFieldChange}
+            fields={[
+              {
+                key: 'content',
+                placeholder: 'Text',
+                type: 'textarea',
+              },
+            ]}
+          />
+          <FieldSet
+            label="Title / Description"
+            onChange={this.onFieldChange}
+            style={styles.fieldset}
+            fields={[
+              {
+                key: 'title',
+                placeholder: 'Title',
+              },
+              {
+                key: 'description',
+                placeholder: 'Description',
+              },
+            ]}
+          />
+          {bottomButton}
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 }
