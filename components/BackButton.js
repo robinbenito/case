1698ca53803036f33ba6b2a2d42a5227c699ab64
Charles Broskoski <broskoski@gmail.com>
2017-08-24 16:37:52 -0400

    Make the BackButton component work

--- components/BackButton.js
+++ components/BackButton.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
+import NavigatorService from '../utilities/navigationService'
+
 import layout from '../constants/Layout'
 
 const styles = StyleSheet.create({
@@ -13,22 +15,14 @@ const styles = StyleSheet.create({
 })
 
 export default class BackButton extends React.Component {
-  goBack() {
-    // if (this.props.navigator.getCurrentIndex() > 0) {
-    //   this.props.navigator.pop()
-    // }
-    return this
-  }
-
   render() {
-  // this.props.navigator.getCurrentIndex() > 0
     return (
       <View onPress={this.goBack} style={styles.container}>
         <Ionicons
           name="ios-arrow-back"
           size={24}
           color="black"
-          onPress={this.goBack}
+          onPress={() => NavigatorService.back()}
           style={{ paddingLeft: layout.padding, paddingRight: layout.padding }}
         />
       </View>
