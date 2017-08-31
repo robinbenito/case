68f4039512f0ce9b649edc172ec2323161c41921
Charles Broskoski <broskoski@gmail.com>
2017-08-31 13:03:08 -0400

    Add header styling

--- navigation/AddStack.js
+++ navigation/AddStack.js
@@ -1,6 +1,8 @@
 import { StackNavigator } from 'react-navigation'
 import { enhance } from 'react-navigation-addons'
 
+import headerStyle from '../constants/Header'
+
 import AddMenuScreen from '../screens/AddScreen'
 import AddTextScreen from '../screens/AddScreen/components/AddTextScreen'
 import AddLinkScreen from '../screens/AddScreen/components/AddLinkScreen'
@@ -11,28 +13,45 @@ import SelectConnectionsScreen from '../components/SelectConnections/index'
 const AddStack = enhance(StackNavigator)({
   addMenu: {
     screen: AddMenuScreen,
+    navigationOptions: () => ({
+      title: 'Add Block',
+      headerStyle,
+    }),
   },
   newChannel: {
     screen: NewChannelScreen,
+    navigationOptions: () => ({
+      title: 'New Channel',
+      headerStyle,
+    }),
   },
   channelVisibility: {
     screen: ChannelVisibility,
+    navigationOptions: () => ({
+      title: 'Channel Privacy',
+      headerStyle,
+    }),
   },
   addLink: {
     screen: AddLinkScreen,
+    navigationOptions: () => ({
+      title: 'Add Link',
+      headerStyle,
+    }),
   },
   addText: {
     screen: AddTextScreen,
+    navigationOptions: () => ({
+      title: 'Add Text',
+      headerStyle,
+    }),
   },
   connect: {
     screen: SelectConnectionsScreen,
   },
 }, {
-  navigationOptions: {
-    headerTintColor: '#000',
-    headerStyle: {
-      backgroundColor: '#fff',
-    },
+  cardStyle: {
+    backgroundColor: '#fff',
   },
 })
 
