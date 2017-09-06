93505a916b8ccbf9ab6b2ffddf99f7e1dcc0acaa
dzucconi <mail@damonzucconi.com>
2017-09-06 16:52:01 -0400

    OK, starting over

---
+++ constants/Style.js
@@ -0,0 +1,33 @@
+export const Colors = {
+  black: 'black',
+  // TODO: Import real values
+  grayBold: '#333',
+  graySemiBold: '#777',
+  grayMedium: '#999',
+  grayRegular: '#ccc',
+  grayLight: '#eee',
+}
+
+export const Units = {
+  base: 10,
+}
+
+export const Border = {
+  borderWidth: 1.5,
+  borderColor: Colors.graySemiBold,
+  borderRadius: 3,
+}
+
+export const Typography = {
+  fontWeight: {
+    normal: 'normal', // 400
+    medium: '500',
+    bold: 'bold', // 700
+  },
+  fontSize: {
+    base: 17,
+  },
+  lineHeight: {
+    base: null, // TODO
+  },
+}
