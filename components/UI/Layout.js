4d7616f046b3c74a1cc5b3598c401d042cfcfce1
dzucconi <mail@damonzucconi.com>
2017-09-18 15:06:36 -0400

    Refines layout components; adds P tag

--- components/UI/Layout.js
+++ components/UI/Layout.js
@@ -15,7 +15,11 @@ export const CenteringPane = styled(KeyboardAvoidingView)`
 
 export const CenterColumn = styled.View`
   width: 50%;
-  margin-horizontal: 25%;
-  flex-direction: row;
+  align-self: center;
+  flex-direction: column;
   justify-content: center;
+`
+
+export const P = styled.View`
+  margin-vertical: ${props => Units.scale[props.space || 1]};
 `
