968d4d41835b1733fe1263800480fcfcd74337d7
dzucconi <mail@damonzucconi.com>
2017-09-20 11:32:13 -0400

    Closer to comps

--- screens/LoggedOutScreen/index.js
+++ screens/LoggedOutScreen/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View } from 'react-native'
 import navigate from '../../utilities/navigationService'
 import { SmallLogo } from '../../components/UI/Logos'
 import { CenteringPane, CenterColumn } from '../../components/UI/Layout'
@@ -8,12 +9,18 @@ export default class LoginScreen extends React.Component {
   render() {
     return (
       <CenteringPane>
-        <SmallLogo />
-
         <CenterColumn>
+          <View
+            alignSelf="center"
+            marginBottom="50%"
+          >
+            <SmallLogo />
+          </View>
+
           <Button space={1} onPress={() => navigate.reset('login')}>
             <ButtonLabel>Log In</ButtonLabel>
           </Button>
+
           <Button space={1} onPress={() => navigate.reset('signUp')}>
             <ButtonLabel>Join</ButtonLabel>
           </Button>
