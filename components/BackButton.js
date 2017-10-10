f91ac2dcbdf8f5e258683c33a0f227fdb84046ea
dzucconi <mail@damonzucconi.com>
2017-10-10 16:25:25 -0400

    Gives <BackButton> a healthy amount of tap area

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import styled from 'styled-components/native'
-
 import NavigatorService from '../utilities/navigationService'
-
 import { Units } from '../constants/Style'
 
 const Container = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding-horizontal: ${Units.scale[1]};
+  padding-left: ${Units.scale[1]};
+  padding-right: ${Units.scale[4]};
+  background-color: transparent;
 `
 
 export default class BackButton extends React.Component {
+  back = () => {
+    NavigatorService.back()
+  }
+
   render() {
     return (
-      <Container onPress={() => NavigatorService.back()}>
+      <Container onPress={this.back}>
         <Ionicons
           name="ios-arrow-back"
           size={24}
           color="black"
-          onPress={() => NavigatorService.back()}
+          onPress={this.back}
           style={{ paddingHorizontal: Units.scale[1] }}
         />
       </Container>
