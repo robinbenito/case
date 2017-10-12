e0645c45a10c12a2be161be278e9517e1663a271
Charles Broskoski <broskoski@gmail.com>
2017-10-12 15:46:16 -0400

    Make NewChannel screen use ChannelForm

--- screens/NewChannelScreen/index.js
+++ screens/NewChannelScreen/index.js
@@ -2,37 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
-
-import {
-  StyleSheet,
-  View,
-} from 'react-native'
-
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import SettingsList from 'react-native-settings-list'
-import { NavigationActions } from 'react-navigation'
 import Store from '../../state/Store'
 import { TOGGLE_ADD_MENU } from '../../state/actions'
 
-import FieldSet from '../../components/FieldSet'
 import BackButton from '../../components/BackButton'
-import HeaderRightButton from '../../components/HeaderRightButton'
+import ChannelForm from '../../components/ChannelForm'
 
 import NavigatorService from '../../utilities/navigationService'
 
-import layout from '../../constants/Layout'
-import colors from '../../constants/Colors'
 import { Colors } from '../../constants/Style'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.gray.background,
-  },
-  fieldset: {
-    marginTop: layout.padding * 2,
-  },
-})
 
 const navigationOptions = {
   title: 'New Channel',
@@ -44,46 +22,8 @@ class NewChannelScreen extends React.Component {
     return navigationOptions
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      description: '',
-      visibility: 'CLOSED',
-    }
-    this.onFieldChange = this.onFieldChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.goToChannelVisibilityScreen = this.goToChannelVisibilityScreen.bind(this)
-    this.onVisibilityChange = this.onVisibilityChange.bind(this)
-  }
-
-  componentDidUpdate() {
-    // Hide or show the done button depending on if content is present
-    if (this.state.title) {
-      this.setNavOptions({
-        headerRight: (
-          <HeaderRightButton onPress={this.onSubmit} text="Save" />
-        ),
-      })
-    } else {
-      this.setNavOptions({
-        headerRight: null,
-      })
-    }
-  }
-
-  onFieldChange(key, value) {
-    this.setState({
-      [key]: value,
-    })
-  }
-
-  onVisibilityChange(value) {
-    this.setState({ visibility: value.toUpperCase() })
-  }
-
-  onSubmit() {
-    this.props.mutate({ variables: this.state }).then((response) => {
+  onSubmit = (variables) => {
+    this.props.mutate({ variables }).then((response) => {
       const { data } = response
       if (!data.error) {
         const { create_channel: { channel: { id, title, visibility } } } = data
@@ -93,54 +33,14 @@ class NewChannelScreen extends React.Component {
     })
   }
 
-  setNavOptions(options) {
-    const newOptions = Object.assign({}, navigationOptions, options)
-    this.props.navigation.setOptions(newOptions)
-  }
-
-  goToChannelVisibilityScreen() {
-    const { visibility } = this.state
-
-    const navigateAction = NavigationActions.navigate({
-      routeName: 'channelVisibility',
-      params: { visibility, onVisibilityChange: this.onVisibilityChange },
-    })
-
-    this.props.navigation.dispatch(navigateAction)
-  }
-
   render() {
-    const { visibility } = this.state
+    const { navigation } = this.props
     return (
-      <KeyboardAwareScrollView style={styles.container}>
-        <View style={styles.container}>
-          <FieldSet
-            isFirst
-            label="Title / Description"
-            onChange={this.onFieldChange}
-            style={styles.fieldset}
-            fields={[
-              {
-                key: 'title',
-                placeholder: 'Title',
-              },
-              {
-                key: 'description',
-                placeholder: 'Description',
-              },
-            ]}
-          />
-          <SettingsList borderColor={colors.gray.background}>
-            <SettingsList.Header headerText="" />
-            <SettingsList.Item
-              title="Privacy"
-              titleInfo={visibility.charAt(0).toUpperCase() + visibility.substr(1).toLowerCase()}
-              titleInfoStyle={{ color: Colors.channel[visibility.toLowerCase()] }}
-              onPress={this.goToChannelVisibilityScreen}
-            />
-          </SettingsList>
-        </View>
-      </KeyboardAwareScrollView>
+      <ChannelForm
+        onSubmit={this.onSubmit}
+        navigation={navigation}
+        navigationOptions={navigationOptions}
+      />
     )
   }
 }
