88f6c9a47709c0fdf16699439f28e231dd67da51
dzucconi <mail@damonzucconi.com>
2017-10-18 17:02:58 -0400

    Refactor this to make sense/use service

--- screens/ProfileScreen/index.js
+++ screens/ProfileScreen/index.js
@@ -1,54 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { AsyncStorage, View } from 'react-native'
 import ProfileContainerWithData from './components/ProfileContainer'
 import { Container } from '../../components/UI/Layout'
+import currentUserService from '../../utilities/currentUserService'
 
 export default class ProfileScreen extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentUser: false,
-      storageSynced: false,
-    }
-  }
-
-  componentWillMount() {
-    this.checkLoginStateAsync()
-  }
-
-  async checkLoginStateAsync() {
-    try {
-      const currentUser = await AsyncStorage.getItem('@arena:CurrentUser')
-      this.setState({ currentUser: JSON.parse(currentUser), storageSynced: true })
-    } catch (e) {
-      this.setState({ currentUser: false, storageSynced: true })
-    }
-  }
-
   render() {
-    if (this.state.storageSynced) {
-      const { navigation } = this.props
-      const profileParam = navigation.state.params && navigation.state.params.id
+    const { navigation } = this.props
+    const profileParam = navigation.state.params && navigation.state.params.id
+    const id = profileParam || currentUserService.sync.get().id
 
-      const id = profileParam || this.state.currentUser.id
-
-      return (
-        <Container>
-          <ProfileContainerWithData
-            id={id}
-            type="CHANNEL"
-            page={1}
-          />
-        </Container>
-      )
-    }
     return (
-      <View />
+      <Container>
+        <ProfileContainerWithData
+          id={id}
+          type="CHANNEL"
+          page={1}
+        />
+      </Container>
     )
   }
 }
 
 ProfileScreen.propTypes = {
-  navigation: PropTypes.any.isRequired,
+  navigation: PropTypes.object.isRequired,
 }
