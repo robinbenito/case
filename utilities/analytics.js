24e115563214077a20702c78736434bbfdd55609
Charles Broskoski <broskoski@gmail.com>
2017-10-18 15:48:01 -0400

    Make linter ignore this file

--- utilities/analytics.js
+++ utilities/analytics.js
@@ -1,3 +1,8 @@
+// Here we require a file that is not checked in, so
+// ignore this error
+/* eslint import/no-unresolved: 0 */
+/* eslint import/extensions: 0 */
+
 import { Analytics, PageHit, Event } from 'expo-analytics'
 import Config from '../config'
 
