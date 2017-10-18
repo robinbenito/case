6e84b3ab20fa3b15264ec3b09164bf04af032bd7
dzucconi <mail@damonzucconi.com>
2017-10-18 18:05:04 -0400

    Implements a HOC to decorate components with loading/error displays

---
+++ components/WithLoadingAndErrors.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ErrorScreen from './ErrorScreen'
+import LoadingScreen from './LoadingScreen'
+
+export default (WrappedComponent, { errorMessage }) =>
+  class extends Component {
+    static propTypes = {
+      data: PropTypes.shape({
+        error: PropTypes.object,
+        loading: PropTypes.bool.isRequired,
+      }).isRequired,
+    }
+
+    static defaultProps = {
+      data: {
+        error: null,
+        loading: true,
+      },
+    }
+
+    render() {
+      const { data: { loading, error } } = this.props
+
+      if (loading) {
+        return <LoadingScreen />
+      }
+
+      if (error) {
+        return (
+          <ErrorScreen
+            message={errorMessage}
+            error={error}
+          />
+        )
+      }
+
+      return <WrappedComponent {...this.props} />
+    }
+  }
