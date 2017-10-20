5d0d34f8c5cdd3f693eb57748a143b65c60de455
dzucconi <mail@damonzucconi.com>
2017-10-20 16:44:18 -0400

    Ensures consistent heights with the two competing headers

--- constants/Header.js
+++ constants/Header.js
@@ -1,9 +1,11 @@
+import { HEADER_HEIGHT } from '../components/Header'
 import { Colors, Typography } from './Style'
 
 export default {
   headerTintColor: Colors.semantic.text,
   headerBackTitle: null,
   headerStyle: {
+    height: HEADER_HEIGHT,
     backgroundColor: 'white',
     borderBottomWidth: 0,
   },
