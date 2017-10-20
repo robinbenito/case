275e599baeb434305ce8cd87c2cdd25768c94d66
Charles Broskoski <broskoski@gmail.com>
2017-10-20 18:19:07 -0400

    Make default header absolute

--- constants/Header.js
+++ constants/Header.js
@@ -6,9 +6,16 @@ export default {
   headerStyle: {
     backgroundColor: 'white',
     borderBottomWidth: 0,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
   },
   headerTitleStyle: {
     fontWeight: Typography.fontWeight.semiBold,
     color: Colors.semantic.text,
+  },
+  cardStyle: {
+    backgroundColor: 'white',
   },
 }
