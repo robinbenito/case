125722843f495c4f0b53190ba639d0521530ca82
dzucconi <mail@damonzucconi.com>
2017-10-23 13:24:57 -0400

    Displays the confirm message on the feed

--- screens/FeedScreen/index.js
+++ screens/FeedScreen/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+
+import { defer } from 'lodash'
+
+import currentUserService from '../../utilities/currentUserService'
+
 import FeedContainer from './components/FeedContainer'
 import { Container } from '../../components/UI/Layout'
+import { sendAlert } from '../../components/Alerts'
 
 export default class FeedScreen extends React.Component {
+  componentDidMount() {
+    defer(() => {
+      if (!currentUserService.sync.get('is_pending_confirmation')) return
+
+      sendAlert({
+        type: 'confirmation',
+        children: 'Please check your email to confirm your account.',
+      })
+    })
+  }
+
   render() {
     return (
       <Container>
