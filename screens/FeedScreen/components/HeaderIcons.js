a8bca5e6d4c580485dbadb86be5b28d9e5272d8e
Charles Broskoski <broskoski@gmail.com>
2017-10-24 09:30:06 -0400

    Update SearchIcon style

--- screens/FeedScreen/components/HeaderIcons.js
+++ screens/FeedScreen/components/HeaderIcons.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
 import PropTypes from 'prop-types'
+import styled from 'styled-components/native'
 
 import NotificationCountWithData from '../../../components/NotificationCount'
 import SearchIcon from '../../../components/SearchIcon'
 
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`
+
 const HeaderIcons = props => (
-  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-    <NotificationCountWithData onPress={() => props.navigation.navigate('DrawerOpen')} />
+  <Container>
     <SearchIcon />
-  </View>
+    <NotificationCountWithData onPress={() => props.navigation.navigate('DrawerOpen')} />
+  </Container>
 )
 
 HeaderIcons.propTypes = {
