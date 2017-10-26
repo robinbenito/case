e7787687eb5189574b6e6045cd3a1aca0ee44ef6
dzucconi <mail@damonzucconi.com>
2017-10-26 14:12:01 -0400

    Updates alerting with queries

--- screens/FeedScreen/index.js
+++ screens/FeedScreen/index.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { gql, graphql } from 'react-apollo'
 
 import FeedContainer from './components/FeedContainer'
 import { Container } from '../../components/UI/Layout'
 import { sendAlert } from '../../components/Alerts'
 
-import currentUserService from '../../utilities/currentUserService'
+class FeedScreen extends React.Component {
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+  }
 
-export default class FeedScreen extends React.Component {
-  componentDidMount() {
-    if (currentUserService.sync.get('is_pending_confirmation')) {
+  componentDidUpdate() {
+    const { data: { loading, error, alerts } } = this.props
+
+    if (loading || error) return
+
+    const { is_confirmed, unconfirmed_email } = alerts
+
+    if (!is_confirmed) {
       sendAlert({
-        id: 'is_pending_confirmation',
+        id: 'feed:unconfirmed_account',
         type: 'confirmation',
-        route: 'feed', // Manually pass in route name, because `feed` is sometimes `main`
         children: 'Please check your email to confirm your account.',
       })
     }
+
+    if (unconfirmed_email) {
+      sendAlert({
+        id: 'feed:unconfirmed_email',
+        type: 'confirmation',
+        children: `Please check your email to confirm ${unconfirmed_email}`,
+      })
+    }
   }
+
   render() {
     return (
       <Container>
@@ -25,3 +43,19 @@ export default class FeedScreen extends React.Component {
     )
   }
 }
+
+const AlertsQuery = gql`
+  query AlertsQuery {
+    alerts: me {
+      id
+      is_confirmed
+      unconfirmed_email
+    }
+  }
+`
+
+const FeedScreenWithData = graphql(AlertsQuery, {
+  fetchPolicy: 'network-only',
+})(FeedScreen)
+
+export default FeedScreenWithData
