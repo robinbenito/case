1f0b61661642ae60f099c355797766eb131e2629
dzucconi <mail@damonzucconi.com>
2017-10-31 10:28:56 -0400

    Avoids erroring if string is null

--- utilities/inflections.js
+++ utilities/inflections.js
@@ -2,4 +2,4 @@ export const pluralize = (count, noun, suffix = 's') =>
   `${count} ${noun}${count !== 1 ? suffix : ''}`
 
 export const capitalize = x =>
-  x[0].toUpperCase() + x.substring(1)
+  x && x[0].toUpperCase() + x.substring(1)
