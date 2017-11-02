fac323f92e97182dcb1ea650b5864da2b34dc207
dzucconi <mail@damonzucconi.com>
2017-11-02 16:26:55 -0400

    Pend everything but canary

--- components/__tests__/ChannelName.js
+++ components/__tests__/ChannelName.js
@@ -10,7 +10,7 @@ const channel = {
   visibility: 'private',
 }
 
-test('renders correctly', () => {
+xtest('renders correctly', () => {
   const tree = shallow(
     <ChannelNameText channel={channel} />,
   )
