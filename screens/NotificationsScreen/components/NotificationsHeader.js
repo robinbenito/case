b0e6a08c8edb98a4393641789478e318065c1091
Charles Broskoski <broskoski@gmail.com>
2017-11-03 15:49:18 -0400

    Refactor NotificationsHeader

--- screens/NotificationsScreen/components/NotificationsHeader.js
+++ screens/NotificationsScreen/components/NotificationsHeader.js
@@ -1,39 +1,33 @@
 import React from 'react'
-
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native'
+import styled from 'styled-components/native'
 
 import NotificationsCount from '../../../components/NotificationCount'
+import { Units, Border } from '../../../constants/Style'
+import { H1 } from '../../../components/UI/Texts'
 
-import layout from '../../../constants/Layout'
-import type from '../../../constants/Type'
-import colors from '../../../constants/Colors'
+const TOP_BAR_HEIGHT = 70
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: layout.topbar - layout.padding,
-    marginTop: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.gray.border,
-  },
-  text: {
-    fontSize: type.sizes.medium,
-  },
-})
+const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: ${TOP_BAR_HEIGHT};
+  margin-top: ${Units.base};
+  border-bottom-width: ${Border.borderWidth};
+  border-bottom-color: ${Border.borderColor};
+  padding-horizontal: ${Units.base};
+`
 
+const Headline = styled(H1)`
+  font-weight: normal;
+`
 export default class NotificationsHeader extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Notifications </Text>
+      <Header>
+        <Headline>Notifications</Headline>
         <NotificationsCount />
-      </View>
+      </Header>
     )
   }
 }
