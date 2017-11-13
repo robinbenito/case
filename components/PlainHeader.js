b077f5f44c5fdef710f8ec88e0b923e8ab00f2f8
dzucconi <mail@damonzucconi.com>
2017-11-13 17:50:35 -0500

    Implements a stripped down version of the header

---
+++ components/PlainHeader.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/native'
+
+import { HEADER_HEIGHT } from './Header'
+
+import { Colors, Typography, Units } from '../constants/Style'
+
+const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  height: ${HEADER_HEIGHT - Units.statusBarHeight}
+`
+
+const Label = styled.Text`
+  font-weight: ${Typography.fontWeight.semiBold};
+  color: ${Colors.semantic.text};
+`
+
+export default class PlainHeader extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  }
+
+  render() {
+    const { children, ...rest } = this.props
+
+    return (
+      <Container {...rest}>
+        <Label>
+          {children}
+        </Label>
+      </Container>
+    )
+  }
+}
