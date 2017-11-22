78945aefb4d3ea8d047bd707d63f5198423781bf
dzucconi <mail@damonzucconi.com>
2017-11-22 14:03:25 -0500

    Allows texts to be created within a channel directly

--- screens/AddTextScreen/index.js
+++ screens/AddTextScreen/index.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { graphql } from 'react-apollo'
 
 import TextForm from '../../components/Form/TextForm'
 
+import createBlockMutation from '../AddConnectionScreen/mutations/createBlock'
+
 import navigationService from '../../utilities/navigationService'
 
-export default class AddTextScreen extends Component {
+class AddTextScreen extends Component {
   static propTypes = {
     navigation: PropTypes.object.isRequired,
+    channel: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    }),
+    createBlock: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
-    navigation: {},
+    channel: null,
   }
 
-  onSubmit = ({ title, description, content }) =>
+  onSubmit = ({ title, description, content }) => {
+    const { channel, createBlock } = this.props
+
+    if (channel) {
+      const promise = createBlock({
+        variables: {
+          title,
+          description,
+          content,
+          channel_ids: [channel.id],
+        },
+      })
+
+      navigationService.navigate('progress', {
+        promise,
+        label: `Connecting to ${channel.title}`,
+        done: () =>
+          navigationService.navigate('channel', channel),
+      })
+
+      return promise
+    }
+
     navigationService.navigate('connect', {
-      title,
-      description,
-      content,
+      title, description, content,
     })
+  }
 
   render() {
     const { navigation } = this.props
@@ -33,3 +63,7 @@ export default class AddTextScreen extends Component {
     )
   }
 }
+
+export default graphql(createBlockMutation, {
+  name: 'createBlock',
+})(AddTextScreen)
