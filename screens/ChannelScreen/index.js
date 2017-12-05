f5eb92f74fec0eff5c8929f0905d09cbc24ceb16
dzucconi <mail@damonzucconi.com>
2017-12-05 09:10:53 -0500

    Passes lint

--- screens/ChannelScreen/index.js
+++ screens/ChannelScreen/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import { Container } from '../../components/UI/Layout'
 import ChannelContainer from './components/ChannelContainer'
