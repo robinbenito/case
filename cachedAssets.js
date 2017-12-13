c53ac4375c98ca76375e4b0c71bf1c9b150e94a6
dzucconi <mail@damonzucconi.com>
2017-12-13 17:51:36 -0500

    Ensure we pull from the index when available

--- cachedAssets.js
+++ cachedAssets.js
@@ -1,16 +1,14 @@
-/* eslint-disable global-require */
+import { values } from 'lodash'
+
+import AddMenuIcons from './components/AddMenu/icons'
+
+const logo = require('./assets/images/logo.png')
+const searchIcon = require('./assets/images/searchIcon.png')
 
 export default {
   images: [
-    require('./assets/images/logo.png'),
-    require('./assets/images/searchIcon.png'),
-    require('./components/AddMenu/icons/add.png'),
-    require('./components/AddMenu/icons/text.png'),
-    require('./components/AddMenu/icons/link.png'),
-    require('./components/AddMenu/icons/image.png'),
-    require('./components/AddMenu/icons/camera.png'),
-    require('./components/AddMenu/icons/cancel.png'),
+    logo,
+    searchIcon,
+    ...values(AddMenuIcons),
   ],
 }
-
-/* eslint-enable global-require */
