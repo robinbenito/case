1a05340fae25f89ce8df95ad4daa3e8f6d0782d6
dzucconi <mail@damonzucconi.com>
2017-12-14 10:44:17 -0500

    `normal` => `regular`; add some weights for context that we are not currently using

--- constants/Style.js
+++ constants/Style.js
@@ -72,10 +72,15 @@ export const Border = {
 
 export const Typography = {
   fontWeight: {
-    normal: '400',
+    // thin: '100',
+    // ultraLight: '200',
+    // light: '300',
+    regular: '400',
     medium: '500',
     semiBold: '600',
     bold: '700',
+    // heavy: '800',
+    // black: '900',
   },
 
   fontSize: {
